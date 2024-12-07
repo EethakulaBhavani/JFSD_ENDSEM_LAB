f9d4ffdde55b23a36cb4db9947581566182e79f7
EethakulaBhavani <eethakulabhavani@gmail.com>
2024-12-07 08:22:40 +0530

    Initial commit

---
+++ src/main/java/com/klef/jfsd/exam/repository/BookRepository.java
@@ -0,0 +1,8 @@
+
+package com.klef.jfsd.exam.repository;
+
+import com.klef.jfsd.exam.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Integer> {
+}
